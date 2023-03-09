2640fd2dd678616be1bb36a75057e151104b20f4
Kirill <ganaboss66@gmail.com>
2023-03-09 12:36:03 +0300

    Задача 8

---
+++ Задача_8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Чётные числа от 1 до n: ");
+for (int i = 2; i <= n; i += 2)
+Console.Write(i + " ");
