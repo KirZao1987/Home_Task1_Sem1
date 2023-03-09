74ce6df329717f498ee023e59033b8c0aeae2a5a
Kirill <ganaboss66@gmail.com>
2023-03-09 11:05:13 +0300

    Задача 2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (n > m)
+  Console.WriteLine(n);
+else if (m > n)
+  Console.WriteLine(m);
+else
+  Console.WriteLine("=");
