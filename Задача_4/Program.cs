bb8505428410abc8a9b57dafd26f8ca39a436569
Kirill <ganaboss66@gmail.com>
2023-03-09 11:48:34 +0300

    Задача 4

---
+++ Задача_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int p = int.Parse(Console.ReadLine()!);
+int max = n;
+if (m > max) {
+  max = m;
+}
+if (p > max) {
+  max = p;
+}
+Console.WriteLine("Максимальное число: " + max);
