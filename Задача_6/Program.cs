b8cd09f1b59cf549092abab1a560768ff1d450dc
Kirill <ganaboss66@gmail.com>
2023-03-09 12:08:14 +0300

    Задача 6

---
+++ Задача_6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n % 2 == 0) 
+  Console.WriteLine("число является чётным.");
+else 
+Console.WriteLine("число не является чётным.");
